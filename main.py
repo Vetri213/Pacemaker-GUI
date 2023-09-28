ae4ba6ee2c65c16d4c81cad98348ee4ad0d5125c
Vetrivel Balaji <vetrivelbalaji333@gmail.com>
2023-09-28 16:03:44 -0400

    Creating Window

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ window.title("First Window")
 # Changing background colour
 # window.configure(background="black")
 
+# Changing window size
+width, height = window.winfo_screenwidth(), window.winfo_screenheight()
+window.geometry('%dx%d+0+0' % (width, height))
 
-print("hello universe")
-print("Hope this works!!!")
-print("Nish is online")
+# Background Image
+#bg = PhotoImage(file="Sad_Background.gif")
+#label1 = Label(window, image=bg)
+#label1.place(x=0, y=0)
+#label1.lower()
+#bg_order = 0
 
-print("nice")
-print("WE GOT ITTT")
+pygame.init()
+
+window.mainloop()
