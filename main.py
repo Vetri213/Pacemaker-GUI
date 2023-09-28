d9732ef95e516713502a34df46db89c80ff4e42e
Vetrivel Balaji <vetrivelbalaji333@gmail.com>
2023-09-28 16:01:00 -0400

    Creating Window

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import pygame
+from tkinter import *
+
+
+# creating and naming window
+window = Tk()
+window.title("First Window")
+# Changing background colour
+# window.configure(background="black")
+
+
 print("hello universe")
 print("Hope this works!!!")
 print("Nish is online")
