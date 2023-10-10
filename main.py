fec676ebb942c0cf9d4c87bc56d07e0339fd3f95
Vetrivel Balaji <vetrivelbalaji333@gmail.com>
2023-10-10 14:21:37 -0400

    Creating Window

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import pygame
 from tkinter import *
 
 
-# creating and naming window
-window = Tk()
-window.title("First Window")
-# Changing background colour
-# window.configure(background="black")
-
-# Changing window size
-width, height = window.winfo_screenwidth(), window.winfo_screenheight()
-window.geometry('%dx%d+0+0' % (width, height))
 
 # Background Image
 #bg = PhotoImage(file="Sad_Background.gif")
@@ -19,6 +10,26 @@ window.geometry('%dx%d+0+0' % (width, height))
 #label1.lower()
 #bg_order = 0
 
-pygame.init()
+if __name__=='__main__':
+    #get users list
+    try:
+        file = open(text.txt,r)
+        #users=pickle.load(open('users.dat','rb')) # read the file of users, if the file does not exist create empty list and the number of users=0
+        #count=len(users)
+    except:
+        users= []
+        count=0
+    print(count)
+    # creating and naming window
+    root = Tk()
+    root.title("Pacemaker GUI")
+    # Changing background colour
+    root.configure(background="black")
 
-window.mainloop()
+    # Changing window size
+    width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.geometry('%dx%d+0+0' % (width, height))
+
+    root.resizable(False, False)
+    #Application(master=root)
+    root.mainloop()
