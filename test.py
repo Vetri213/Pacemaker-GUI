58d0f029a0845419705b4bdc24ed4970c82d3096
Vetrivel Balaji <vetrivelbalaji333@gmail.com>
2023-11-28 15:40:52 -0500

    Serial Port Test File

--- test.py
+++ test.py
@@ -3,3 +3,15 @@ ports = serial.tools.list_ports.comports()
 
 for port, desc, hwid in sorted(ports):
         print("{}: {} [{}]".format(port, desc, hwid))
+
+try:
+        serial.Serial(port="COM4", baudrate=115200)
+        print("Connected to 4")
+except:
+        print("No Connection")
+
+try:
+        serial.Serial(port="COM3", baudrate=115200)
+        print("Connected to 3")
+except:
+        print("No Connection 3")
